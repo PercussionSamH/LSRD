5568a2ef6504a5ddd10d94f09570b6f54590c1fb
percussionSamH <percussionsamuel@gmail.com>
2026-01-28 21:09:48 -0500

    changes to form1 connecting plc

--- LSRD_hmi/Form1.Designer.cs
+++ LSRD_hmi/Form1.Designer.cs
@@ -28,36 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.PB_doorman_mode = new System.Windows.Forms.Button();
             this.PB_wave = new System.Windows.Forms.Button();
+            this.timer_Modbus_Com = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // PB_doorman_mode
             // 
-            this.PB_doorman_mode.Location = new System.Drawing.Point(192, 216);
+            this.PB_doorman_mode.Location = new System.Drawing.Point(256, 266);
+            this.PB_doorman_mode.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.PB_doorman_mode.Name = "PB_doorman_mode";
-            this.PB_doorman_mode.Size = new System.Drawing.Size(171, 88);
+            this.PB_doorman_mode.Size = new System.Drawing.Size(228, 108);
             this.PB_doorman_mode.TabIndex = 0;
             this.PB_doorman_mode.Text = "Doorman Mode";
             this.PB_doorman_mode.UseVisualStyleBackColor = true;
             // 
             // PB_wave
             // 
-            this.PB_wave.Location = new System.Drawing.Point(437, 216);
+            this.PB_wave.Location = new System.Drawing.Point(583, 266);
+            this.PB_wave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.PB_wave.Name = "PB_wave";
-            this.PB_wave.Size = new System.Drawing.Size(171, 88);
+            this.PB_wave.Size = new System.Drawing.Size(228, 108);
             this.PB_wave.TabIndex = 1;
             this.PB_wave.Text = "Wave";
             this.PB_wave.UseVisualStyleBackColor = true;
             this.PB_wave.Click += new System.EventHandler(this.PB_wave_Click);
             // 
+            // timer_Modbus_Com
+            // 
+            this.timer_Modbus_Com.Tick += new System.EventHandler(this.timer_Modbus_Com_Tick);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.PB_wave);
             this.Controls.Add(this.PB_doorman_mode);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -68,6 +77,7 @@
 
         private System.Windows.Forms.Button PB_doorman_mode;
         private System.Windows.Forms.Button PB_wave;
+        private System.Windows.Forms.Timer timer_Modbus_Com;
     }
 }
 
--- LSRD_hmi/Form1.cs
+++ LSRD_hmi/Form1.cs
@@ -1,32 +1,109 @@
-﻿using System;
+﻿//Library includes
+using EasyModbus;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-using EasyModbus;
 
 
 namespace LSRD_hmi
 {
+
     public partial class Form1 : Form
     {
+        //IP address
+        static string PLC_IP = "10.104.5.184"; static int port = 502;
+        public ModbusClient modbusClient = new ModbusClient(PLC_IP, port);
+
+        //Bit ranges
+        bool[] QX1; //QX1.0 - QX1.9
+
+
         public Form1()
         {
             InitializeComponent();
-            //intialize modbus
-            ModbusClient modbusClient = new ModbusClient("10.104.5.184", 502);
-            modbusClient.Connect();
+            try
+            {
+                System.Diagnostics.Debug.WriteLine("Connecting to " + PLC_IP + " on port " + port);
+                modbusClient.Connect();
+
+                System.Diagnostics.Debug.WriteLine("Connected!");
+
+                ////start timer for push/pull sync
+                //timer_Modbus_Com.Enabled = true;
+
+                //read initial QX vars
+                QX1 = modbusClient.ReadCoils(1, 10); //all QX1.X vars
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Connection to " + PLC_IP + " on port " + port + " failed...");
+                System.Diagnostics.Debug.WriteLine("ERROR: ", ex.Message);
+                throw;
+            }
         }
 
         private void PB_wave_Click(object sender, EventArgs e)
         {
-            
-            modbusClient.WriteSingleCoil(0, true);
+            //bool PB_mode_wave;
+            //PB_mode_wave = true;
+
+            for (int i = 0; i < QX1.Length; i++)
+            {
+                Console.WriteLine("QX1." + i + " is currently: " + QX1[i]);
+            }
+            //Set vars to bits
+            QX1[0] = true;
+
+            //Write to PLC
+            //modbusClient.WriteMultipleCoils(1, QX1);
+
+            modbusClient.WriteMultipleCoils(4, QX1);///I don't know why this is on coil 4, it doesn't make any sense
+            for (int i = 0; i < QX1.Length; i++)
+            {
+                Console.WriteLine("QX1." + i + " is  now: " + QX1[i]);
+            }
+
+            //--------------------
+            //QX1 = modbusClient.ReadCoils(1, 10);
+            //Console.WriteLine("bit 1.3" + " set to: " + QX1[3]);
+
+            //Thread.Sleep(2000);
+            //modbusClient.WriteMultipleCoils(1, new bool[] { true, true, true, true, true, true});
+            //QX1 = modbusClient.ReadCoils(1, 10);
+            //Console.WriteLine("bit 1.3" + " read as: " + QX1[3]);
+            //for (int i = 0; i < QX1.Length; i++)
+            //{
+            //   Console.WriteLine("QX1." + i + " set to: " + QX1[i]);
+            //}
+            //Console.WriteLine("QX1." + "3" + " set to: " + QX1[3]);
+
+
+        }
+
+
+        private void timer_Modbus_Com_Tick(object sender, EventArgs e)
+        {
+            //Read inputs
+            //QX1 = modbusClient.ReadCoils(1, 10);
+
+
+
+            //timer_Modbus_Com.Enabled = false;//prevents multiple signals within value
+            ////grab values
+            //modbusClient.WriteMultipleCoils(4, new bool[] { true, true, true, true, true, true, true, true, true, true });    //Write Coils starting with Address 5
+            //bool[] readCoils = modbusClient.ReadCoils(9, 10);                        //Read 10 Coils from Server, starting with address 10
+            //int[] readHoldingRegisters = modbusClient.ReadHoldingRegisters(0, 10);    //Read 10 Holding Registers from Server, starting with Address 1
+
+            ////myvar = readHoldingRegisters[0];
+            //timer_Modbus_Com.Enabled = true;
         }
     }
 }
