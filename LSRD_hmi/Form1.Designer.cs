14156aea1dd844eeade6767789b1c6812c26f0ef
percussionSamH <percussionsamuel@gmail.com>
2026-02-11 17:21:42 -0500

    new background and buttons

--- LSRD_hmi/Form1.Designer.cs
+++ LSRD_hmi/Form1.Designer.cs
@@ -30,54 +30,112 @@
         {
             this.components = new System.ComponentModel.Container();
             this.PB_doorman_mode = new System.Windows.Forms.Button();
-            this.PB_wave = new System.Windows.Forms.Button();
             this.timer_Modbus_Com = new System.Windows.Forms.Timer(this.components);
+            this.PB_wave = new System.Windows.Forms.Button();
+            this.PB_Draw_Fish1 = new System.Windows.Forms.Button();
+            this.PB_Draw_Square = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.test_textbox = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // PB_doorman_mode
             // 
-            this.PB_doorman_mode.Location = new System.Drawing.Point(256, 266);
-            this.PB_doorman_mode.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.PB_doorman_mode.BackColor = System.Drawing.Color.Gainsboro;
+            this.PB_doorman_mode.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.PB_doorman_mode.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.PB_doorman_mode.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.PB_doorman_mode.Location = new System.Drawing.Point(58, 26);
+            this.PB_doorman_mode.Margin = new System.Windows.Forms.Padding(4);
             this.PB_doorman_mode.Name = "PB_doorman_mode";
-            this.PB_doorman_mode.Size = new System.Drawing.Size(228, 108);
+            this.PB_doorman_mode.Size = new System.Drawing.Size(189, 92);
             this.PB_doorman_mode.TabIndex = 0;
-            this.PB_doorman_mode.Text = "Doorman Mode";
-            this.PB_doorman_mode.UseVisualStyleBackColor = true;
-            // 
-            // PB_wave
-            // 
-            this.PB_wave.Location = new System.Drawing.Point(583, 266);
-            this.PB_wave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.PB_wave.Name = "PB_wave";
-            this.PB_wave.Size = new System.Drawing.Size(228, 108);
-            this.PB_wave.TabIndex = 1;
-            this.PB_wave.Text = "Wave";
-            this.PB_wave.UseVisualStyleBackColor = true;
-            this.PB_wave.Click += new System.EventHandler(this.PB_wave_Click);
+            this.PB_doorman_mode.Text = "Doorman\r\nMode";
+            this.PB_doorman_mode.UseVisualStyleBackColor = false;
+            this.PB_doorman_mode.Visible = false;
             // 
             // timer_Modbus_Com
             // 
             this.timer_Modbus_Com.Tick += new System.EventHandler(this.timer_Modbus_Com_Tick);
             // 
+            // PB_wave
+            // 
+            this.PB_wave.Location = new System.Drawing.Point(58, 126);
+            this.PB_wave.Margin = new System.Windows.Forms.Padding(4);
+            this.PB_wave.Name = "PB_wave";
+            this.PB_wave.Size = new System.Drawing.Size(189, 108);
+            this.PB_wave.TabIndex = 1;
+            this.PB_wave.Text = "Wave";
+            this.PB_wave.UseVisualStyleBackColor = true;
+            this.PB_wave.Visible = false;
+            this.PB_wave.Click += new System.EventHandler(this.PB_wave_Click);
+            // 
+            // PB_Draw_Fish1
+            // 
+            this.PB_Draw_Fish1.BackColor = System.Drawing.Color.Silver;
+            this.PB_Draw_Fish1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.PB_Draw_Fish1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.PB_Draw_Fish1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.PB_Draw_Fish1.Location = new System.Drawing.Point(489, 373);
+            this.PB_Draw_Fish1.Margin = new System.Windows.Forms.Padding(4);
+            this.PB_Draw_Fish1.Name = "PB_Draw_Fish1";
+            this.PB_Draw_Fish1.Size = new System.Drawing.Size(189, 92);
+            this.PB_Draw_Fish1.TabIndex = 2;
+            this.PB_Draw_Fish1.Text = "Draw Fish One";
+            this.PB_Draw_Fish1.UseVisualStyleBackColor = false;
+            this.PB_Draw_Fish1.Click += new System.EventHandler(this.PB_Draw_Fish1_Click);
+            // 
+            // PB_Draw_Square
+            // 
+            this.PB_Draw_Square.BackColor = System.Drawing.Color.LightGray;
+            this.PB_Draw_Square.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.PB_Draw_Square.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.PB_Draw_Square.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.PB_Draw_Square.Location = new System.Drawing.Point(726, 373);
+            this.PB_Draw_Square.Margin = new System.Windows.Forms.Padding(4);
+            this.PB_Draw_Square.Name = "PB_Draw_Square";
+            this.PB_Draw_Square.Size = new System.Drawing.Size(189, 92);
+            this.PB_Draw_Square.TabIndex = 3;
+            this.PB_Draw_Square.Text = "Draw Square";
+            this.PB_Draw_Square.UseVisualStyleBackColor = false;
+            this.PB_Draw_Square.Click += new System.EventHandler(this.PB_Draw_Square_Click);
+            // 
+            // test_textbox
+            // 
+            this.test_textbox.AutoSize = true;
+            this.test_textbox.Location = new System.Drawing.Point(254, 68);
+            this.test_textbox.Name = "test_textbox";
+            this.test_textbox.Size = new System.Drawing.Size(44, 16);
+            this.test_textbox.TabIndex = 4;
+            this.test_textbox.Text = "label1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::LSRD_hmi.Properties.Resources.slideshow_texture_dark_blue;
             this.ClientSize = new System.Drawing.Size(1067, 554);
-            this.Controls.Add(this.PB_wave);
+            this.Controls.Add(this.test_textbox);
+            this.Controls.Add(this.PB_Draw_Square);
+            this.Controls.Add(this.PB_Draw_Fish1);
             this.Controls.Add(this.PB_doorman_mode);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Controls.Add(this.PB_wave);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button PB_doorman_mode;
-        private System.Windows.Forms.Button PB_wave;
         private System.Windows.Forms.Timer timer_Modbus_Com;
+        private System.Windows.Forms.Button PB_wave;
+        private System.Windows.Forms.Button PB_Draw_Fish1;
+        private System.Windows.Forms.Button PB_Draw_Square;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label test_textbox;
     }
 }
 
--- LSRD_hmi/Form1.cs
+++ LSRD_hmi/Form1.cs
@@ -23,12 +23,14 @@ namespace LSRD_hmi
         public ModbusClient modbusClient = new ModbusClient(PLC_IP, port);
 
         //Bit ranges
-        bool[] QX1; //QX1.0 - QX1.9
-
-
+        bool[] QX_Coils; //QX0.0 - QX...
+        int QX_length = 100; //total # of vars
+   
+        //Vars
         public Form1()
         {
             InitializeComponent();
+
             try
             {
                 System.Diagnostics.Debug.WriteLine("Connecting to " + PLC_IP + " on port " + port);
@@ -36,11 +38,12 @@ namespace LSRD_hmi
 
                 System.Diagnostics.Debug.WriteLine("Connected!");
 
-                ////start timer for push/pull sync
-                //timer_Modbus_Com.Enabled = true;
+                //start timer for push/pull sync
+                timer_Modbus_Com.Enabled = true;
 
                 //read initial QX vars
-                QX1 = modbusClient.ReadCoils(1, 10); //all QX1.X vars
+                QX_Coils = modbusClient.ReadCoils(0, QX_length); //read all variables off of PLC
+        
             }
             catch (Exception ex)
             {
@@ -52,58 +55,34 @@ namespace LSRD_hmi
 
         private void PB_wave_Click(object sender, EventArgs e)
         {
-            //bool PB_mode_wave;
-            //PB_mode_wave = true;
-
-            for (int i = 0; i < QX1.Length; i++)
-            {
-                Console.WriteLine("QX1." + i + " is currently: " + QX1[i]);
-            }
-            //Set vars to bits
-            QX1[0] = true;
-
-            //Write to PLC
-            //modbusClient.WriteMultipleCoils(1, QX1);
-
-            modbusClient.WriteMultipleCoils(4, QX1);///I don't know why this is on coil 4, it doesn't make any sense
-            for (int i = 0; i < QX1.Length; i++)
-            {
-                Console.WriteLine("QX1." + i + " is  now: " + QX1[i]);
-            }
-
-            //--------------------
-            //QX1 = modbusClient.ReadCoils(1, 10);
-            //Console.WriteLine("bit 1.3" + " set to: " + QX1[3]);
-
-            //Thread.Sleep(2000);
-            //modbusClient.WriteMultipleCoils(1, new bool[] { true, true, true, true, true, true});
-            //QX1 = modbusClient.ReadCoils(1, 10);
-            //Console.WriteLine("bit 1.3" + " read as: " + QX1[3]);
-            //for (int i = 0; i < QX1.Length; i++)
-            //{
-            //   Console.WriteLine("QX1." + i + " set to: " + QX1[i]);
-            //}
-            //Console.WriteLine("QX1." + "3" + " set to: " + QX1[3]);
-
 
         }
 
 
         private void timer_Modbus_Com_Tick(object sender, EventArgs e)
         {
+
+            //prevents multiple timer signals per read
+            timer_Modbus_Com.Enabled = false;
+
             //Read inputs
-            //QX1 = modbusClient.ReadCoils(1, 10);
+            QX_Coils = modbusClient.ReadCoils(0, QX_length);
+            test_textbox.Text = string.Join(" ", QX_Coils.Select(b => b.ToString())); ;
+            //Reenable timer
+            timer_Modbus_Com.Enabled = true;
+        }
 
+        private void PB_Draw_Fish1_Click(object sender, EventArgs e)
+        {
+            //QX_Coils[16] = true; //set 2.0
+            modbusClient.WriteSingleCoil(16, true);
+        
+        }
 
-
-            //timer_Modbus_Com.Enabled = false;//prevents multiple signals within value
-            ////grab values
-            //modbusClient.WriteMultipleCoils(4, new bool[] { true, true, true, true, true, true, true, true, true, true });    //Write Coils starting with Address 5
-            //bool[] readCoils = modbusClient.ReadCoils(9, 10);                        //Read 10 Coils from Server, starting with address 10
-            //int[] readHoldingRegisters = modbusClient.ReadHoldingRegisters(0, 10);    //Read 10 Holding Registers from Server, starting with Address 1
-
-            ////myvar = readHoldingRegisters[0];
-            //timer_Modbus_Com.Enabled = true;
+        private void PB_Draw_Square_Click(object sender, EventArgs e)
+        {
+            //QX_Coils[17] = true; //set 2.1
+            modbusClient.WriteSingleCoil(17, true);
         }
     }
 }
